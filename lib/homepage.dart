e9c6dcbfc9493d1b29b541827e6da79d16cedbb0
Dxmien <brightobesi@gmail.com>
2024-10-20 07:34:05 +0100

    Tem API

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
@@ -8,6 +10,18 @@ import 'package:http/http.dart' as http;
 import 'package:weather/extra.dart';
 export 'homepage.dart';
 
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: Center(
+      child: CircularProgressIndicator(),
+    ));
+  }
+}
+
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
@@ -16,6 +30,7 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  late double temp = 0;
   @override
   void initState() {
     super.initState();
@@ -23,11 +38,20 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   Future getWeather() async {
-    String cityName = "London";
-    final result = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$cityName,uk&APPID=$opeenWeatherApi'));
-
-    print(result.body);
+    try {
+      String cityName = "London";
+      final result = await http.get(Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName,uk&APPID=$opeenWeatherApi'));
+      final data = jsonDecode(result.body);
+      if (data['cod'] != '200') {
+        throw "An Error Occured";
+      }
+      setState(() {
+        temp = data['list'][0]['main']['temp'];
+      });
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   @override
@@ -60,121 +84,125 @@ class _WeatherScreenState extends State<WeatherScreen> {
             left: 8,
             right: 8,
           ),
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)),
-                  elevation: 50,
-                  child: const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          "26°C",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                            color: CupertinoColors.white,
+          child: temp == 0
+              ? LoadingScreen()
+              : Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        elevation: 50,
+                        child: Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                "$temp °C",
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: CupertinoColors.white,
+                                ),
+                              ),
+                              Icon(
+                                CupertinoIcons.cloud_fill,
+                                size: 70,
+                                color: CupertinoColors.white,
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "Cloudy",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Icon(
-                          CupertinoIcons.cloud_fill,
-                          size: 70,
-                          color: CupertinoColors.white,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          "Cloudy",
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "Weather Forecast",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: const Row(
+                        children: [
+                          DailyForecast(
+                            day: "Monday",
+                            icon: CupertinoIcons.cloud_fill,
+                            time: "10:50",
                           ),
-                        ),
-                      ],
+                          DailyForecast(
+                            day: "Tuesday",
+                            icon: CupertinoIcons.cloud_sun_fill,
+                            time: "09:00",
+                          ),
+                          DailyForecast(
+                            day: "Monday",
+                            icon: CupertinoIcons.cloud_rain_fill,
+                            time: "10:50",
+                          ),
+                          DailyForecast(
+                            day: "Monday",
+                            icon: CupertinoIcons.cloud_sun_bolt_fill,
+                            time: "10:50",
+                          ),
+                          DailyForecast(
+                            day: "Monday",
+                            icon: CupertinoIcons.cloud_rain_fill,
+                            time: "10:50",
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  "Weather Forecast",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: const Row(
-                  children: [
-                    DailyForecast(
-                      day: "Monday",
-                      icon: CupertinoIcons.cloud_fill,
-                      time: "10:50",
+                    const SizedBox(height: 40),
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "Additional Information",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                    DailyForecast(
-                      day: "Tuesday",
-                      icon: CupertinoIcons.cloud_sun_fill,
-                      time: "09:00",
-                    ),
-                    DailyForecast(
-                      day: "Monday",
-                      icon: CupertinoIcons.cloud_rain_fill,
-                      time: "10:50",
-                    ),
-                    DailyForecast(
-                      day: "Monday",
-                      icon: CupertinoIcons.cloud_sun_bolt_fill,
-                      time: "10:50",
-                    ),
-                    DailyForecast(
-                      day: "Monday",
-                      icon: CupertinoIcons.cloud_rain_fill,
-                      time: "10:50",
-                    ),
+                    const SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          AddInfo(
+                            icon: CupertinoIcons.drop_fill,
+                            title: "Humidity",
+                            value: "80",
+                          ),
+                          AddInfo(
+                            icon: CupertinoIcons.wind,
+                            title: "Wind Speed",
+                            value: "89",
+                          ),
+                          AddInfo(
+                            icon: CupertinoIcons.gauge,
+                            title: "Pressure",
+                            value: "100",
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-              ),
-              const SizedBox(height: 40),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  "Additional Information",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    AddInfo(
-                      icon: CupertinoIcons.drop_fill,
-                      title: "Humidity",
-                      value: "80",
-                    ),
-                    AddInfo(
-                      icon: CupertinoIcons.wind,
-                      title: "Wind Speed",
-                      value: "89",
-                    ),
-                    AddInfo(
-                      icon: CupertinoIcons.gauge,
-                      title: "Pressure",
-                      value: "100",
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
         ),
       ),
     );
